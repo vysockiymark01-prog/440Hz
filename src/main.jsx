4f625d704c61c1e03c9f4042fd33b5dfd7d28bdd
vysockiymark01-prog <vysockiymark01@gmail.com>
2026-07-26 21:00:39 +0500

    Wrap App component with PasswordGate

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,18 @@ import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './contexts/ThemeContext.jsx'
 import { FontScaleProvider } from './contexts/FontScaleContext.jsx'
+import PasswordGate from './components/PasswordGate.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider>
-      <FontScaleProvider>
-        <HashRouter>
-          <App />
-        </HashRouter>
-      </FontScaleProvider>
-    </ThemeProvider>
+    <PasswordGate>
+      <ThemeProvider>
+        <FontScaleProvider>
+          <HashRouter>
+            <App />
+          </HashRouter>
+        </FontScaleProvider>
+      </ThemeProvider>
+    </PasswordGate>
   </StrictMode>,
 )
